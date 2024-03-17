088544633307b11b3daec428ffda58c920269d4b
saurabh gharat <saurabhgharat913@gmail.com>
2024-03-17 17:17:08 +0530

    changes

--- src/Pages/AddQuotation.js
+++ src/Pages/AddQuotation.js
@@ -14,6 +14,7 @@ const AddQuotation = () => {
   const openFormTab = (tabId) => {
     setSelectedTab(tabId);
   };
+
   
   const selectForm = (action, selectedTab) => {
     switch (selectedTab) {
