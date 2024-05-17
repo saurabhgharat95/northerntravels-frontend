4a694d9a4e23d9029f7ca91631fdf9cefacf7287
saurabh gharat <saurabhgharat913@gmail.com>
2024-05-17 19:45:42 +0530

    added homepage to package.json

--- src/App.js
+++ src/App.js
@@ -23,49 +23,49 @@ import TourDetails from "./Pages/TourDetails";
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route exact path={`/`} element={<Homepage />} />
-          <Route
-            exact
-            path={`${process.env.PUBLIC_URL}/`}
-            element={<Homepage />}
-          />
-          <Route exact path={`/login`} element={<LoginPage />} />
-          <Route exact path={`/countries`} element={<CountryMasterPage />} />
-          <Route exact path={`/states`} element={<StateMaster />} />
-          <Route exact path={`/transit-pts`} element={<TransitPointMaster />} />
-          <Route exact path={`/destinations`} element={<DestinationMaster />} />
-          <Route exact path={`/hotel-type`} element={<HotelTypeMaster />} />
-          <Route
-            exact
-            path={`/halting-dest`}
-            element={<HaltingDestinationMaster />}
-          />
-          <Route exact path={`/hotels`} element={<HotelMaster />} />
-          <Route exact path={`/cars`} element={<CarMaster />} />
-          <Route
-            exact
-            path={`/transportations`}
-            element={<TransportationMaster />}
-          />
-          <Route exact path={`/meal-types`} element={<MealTypeMaster />} />
-          <Route exact path={`/room-types`} element={<RoomTypeMaster />} />
-          <Route exact path={`/quotations`} element={<QuotationManagement />} />
-          <Route exact path={`/leads`} element={<LeadManagement />} />
-          <Route exact path={`/add-quotation`} element={<AddQuotation />} />
-          <Route exact path={`/edit-quotation/:id?`} element={<AddQuotation />} />
-          <Route
-            exact
-            path={`/maintain-rooms`}
-            element={<HotelRoomManagement />}
-          />
-          <Route exact path={`/tours`} element={<TourManagement />} />
-          <Route exact path={`/add-tour?`} element={<AddTourForm />} />
-          <Route exact path={`/edit-tour/:id?`} element={<AddTourForm />} />
-          <Route exact path={`/tour-details/:id?`} element={<TourDetails />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path={`/`} element={<Homepage />} />
+        <Route
+          exact
+          path={`${process.env.PUBLIC_URL}/`}
+          element={<Homepage />}
+        />
+        <Route exact path={`/login`} element={<LoginPage />} />
+        <Route exact path={`/countries`} element={<CountryMasterPage />} />
+        <Route exact path={`/states`} element={<StateMaster />} />
+        <Route exact path={`/transit-pts`} element={<TransitPointMaster />} />
+        <Route exact path={`/destinations`} element={<DestinationMaster />} />
+        <Route exact path={`/hotel-type`} element={<HotelTypeMaster />} />
+        <Route
+          exact
+          path={`/halting-dest`}
+          element={<HaltingDestinationMaster />}
+        />
+        <Route exact path={`/hotels`} element={<HotelMaster />} />
+        <Route exact path={`/cars`} element={<CarMaster />} />
+        <Route
+          exact
+          path={`/transportations`}
+          element={<TransportationMaster />}
+        />
+        <Route exact path={`/meal-types`} element={<MealTypeMaster />} />
+        <Route exact path={`/room-types`} element={<RoomTypeMaster />} />
+        <Route exact path={`/quotations`} element={<QuotationManagement />} />
+        <Route exact path={`/leads`} element={<LeadManagement />} />
+        <Route exact path={`/add-quotation`} element={<AddQuotation />} />
+        <Route exact path={`/edit-quotation/:id?`} element={<AddQuotation />} />
+        <Route
+          exact
+          path={`/maintain-rooms`}
+          element={<HotelRoomManagement />}
+        />
+        <Route exact path={`/tours`} element={<TourManagement />} />
+        <Route exact path={`/add-tour`} element={<AddTourForm />} />
+        <Route exact path={`/edit-tour/:id?`} element={<AddTourForm />} />
+        <Route exact path={`/tour-details/:id?`} element={<TourDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
